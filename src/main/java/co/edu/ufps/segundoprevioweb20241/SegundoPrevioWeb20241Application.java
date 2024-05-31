7d710c02db8f6bda5c931b1a56f0f53a991994be
Monrrad <ramondavidmc@ufps.edu.co>
2024-05-30 22:25:15 -0500

    Inicialización

---
+++ src/main/java/co/edu/ufps/segundoprevioweb20241/SegundoPrevioWeb20241Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.segundoprevioweb20241;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoPrevioWeb20241Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegundoPrevioWeb20241Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/ufps/segundoprevioweb20241/SegundoPrevioWeb20241ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.segundoprevioweb20241;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoPrevioWeb20241ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
